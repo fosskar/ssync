d1e3d410551784ff64324259b65351dbd3d8a1eb
fosskar <117449098+fosskar@users.noreply.github.com>
2026-08-02 05:51:03 +0000

    nixbot: configure updater checkout promisor

--- nix/effects.nix
+++ nix/effects.nix
@@ -56,6 +56,9 @@ in
         git config --global user.name 'fosskar[bot]'
         git config --global user.email '300917551+fosskar[bot]@users.noreply.github.com'
 
+        git config remote.origin.promisor true
+        git config remote.origin.partialclonefilter blob:none
+
         nix run "github:fosskar/nixfiles#updater-flake-inputs"
       '';
     };
